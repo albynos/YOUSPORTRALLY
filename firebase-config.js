f2c3e5a50b9ab87d8c25cb2d46be5d0da26a3b2e
albynos <albynos@gmail.com>
2025-03-11 17:41:18 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,20 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDr8SIWOMC9HjPzb4iR6vCFc5ONxUa4dGQ",
     authDomain: "yousport-rally.firebaseapp.com",
     projectId: "yousport-rally",
     storageBucket: "yousport-rally.firebasestorage.app",
     messagingSenderId: "1080944459276",
-    appId: "1:1080944459276:web:4a1b509238b5e40f8f7829",
+    appId: "YOUR_APP_ID",
+    measurementId: "1:1080944459276:web:4a1b509238b5e40f8f7829"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
