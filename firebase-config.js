c093a46c256cee444c09ceec95151f9f947f3cee
albynos <albynos@gmail.com>
2025-03-12 11:21:39 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,10 @@ const firebaseConfig = {
     projectId: "yousport-rally",
     storageBucket: "yousport-rally.firebasestorage.app",
     messagingSenderId: "1080944459276",
-    appId: "YOUR_APP_ID",
+    appId: "YOUR_APP_ID", // Inserisci il tuo App ID qui
     measurementId: "1:1080944459276:web:4a1b509238b5e40f8f7829"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-
